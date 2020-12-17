000a56a09a946daf070450622af70380afb20c6b
Miguel Angel Moreno <izzyboy.m@gmail.com>
2020-12-16 18:52:18 -0600

    Add files via upload
    Para que funcione es necesario descomprimir el archivo fullcalendar-4.3.1.zip en el mismo directorio donde están los .html y asi puedan acceder a la carpeta packages.
    El archivo calendario.sql es el script para crear la basede datos que guarda las citas.
    El archivo eventos.php lleva todo el trabajo de actualizar la base de datos

---
+++ screen/CalendarioWeb/eventos.php
@@ -0,0 +1,83 @@
+<?php
+header('Content-Type: application/json');
+$pdo= new PDO("mysql:dbname=sistema;host=127.0.0.1","root","1234");
+
+
+$accion=(isset($_GET['accion']))?$_GET['accion']:'leer';
+switch($accion){
+    case 'agregar':
+        //Instruccion agregar
+        $sentenciaSQL = $pdo->prepare("INSERT INTO
+        eventos(title,descripcion,color,textColor,start,end)
+        VALUES(:title,:descripcion,:color,:textColor,:start,:end)");
+
+        $respuesta=$sentenciaSQL->execute(array(
+                "title" =>$_POST['title'],
+                "descripcion" => $_POST['descripcion'],
+                "color" => $_POST['color'],
+                "textColor" => $_POST['textColor'],
+                "start" => $_POST['start'],
+                "end" => $_POST['end']
+        ));
+        echo json_encode($respuesta);
+        break;
+    case 'eliminar':
+         //Instruccion eliminar
+         //echo "Instruccion eliminar";
+         $respuesta = false;
+         
+         if(isset($_POST['id'])){
+
+                $sentenciaSQL = $pdo->prepare("DELETE FROM eventos WHERE ID=:ID");
+                $respuesta = $sentenciaSQL->execute(array("ID"=>$_POST['id']));
+         }
+         echo json_encode($respuesta);
+
+        break;
+    case 'modificar':
+        //Instruccion modificar
+        //echo "Instruccion modificar";
+
+        $sentenciaSQL = $pdo->prepare("UPDATE eventos SET 
+        title=:title,
+        descripcion=:descripcion,
+        color=:color,
+        textColor=:textColor,
+        start=:start,
+        end=:end
+        WHERE ID=:ID
+         ");
+
+
+        $respuesta=$sentenciaSQL->execute(array(
+            "ID"=>$_POST['id'],
+            "title" =>$_POST['title'],
+            "descripcion" => $_POST['descripcion'],
+            "color" => $_POST['color'],
+            "textColor" => $_POST['textColor'],
+            "start" => $_POST['start'],
+            "end" => $_POST['end']
+        ));
+        echo json_encode($respuesta);
+
+        break;        
+    default:
+        //Seleccionar los eventos del calendario
+        $sentenciaSQL= $pdo ->prepare("SELECT * FROM eventos");
+        $sentenciaSQL ->execute();
+
+        $resultado= $sentenciaSQL ->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($resultado);
+    
+        break;
+
+
+
+
+
+}
+
+
+
+
+?>
